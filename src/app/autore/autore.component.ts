612d535610960a3e7c57b130841ecc87f98ad449
Pasquale <pasquale_florio@live.it>
2020-06-17 16:57:59 +0200

    ngFor and ngSwitch
    ref

--- src/app/autore/autore.component.ts
+++ src/app/autore/autore.component.ts
@@ -10,7 +10,16 @@ export class AutoreComponent implements OnInit {
   constructor() {
   }
 
-  libri = ['libro 1', 'libro 2'];
+  libri = [
+    {id: 1, nome: 'Manzoni'},
+    {id: 2, nome: 'Alighieri'},
+    {id: 3, nome: 'Pasolini'}
+  ];
+
+  // per ngSwitch
+  dietSelection = 'gf';
+  // dietSelection = 'veg';
+  // dietSelection = 'paleo';
 
   ngOnInit() {
   }
