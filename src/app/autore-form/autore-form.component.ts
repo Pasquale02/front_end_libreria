cfdebbaeef5ef91b463207576b336a09e665283f
Pasquale <pasquale_florio@live.it>
2020-06-18 10:54:51 +0200

    validation autore
    autore validation

--- src/app/autore-form/autore-form.component.ts
+++ src/app/autore-form/autore-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-autore-form',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutoreFormComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  log(nomeAutore) {
+    console.log('autore: ', nomeAutore);
   }
 
 }
