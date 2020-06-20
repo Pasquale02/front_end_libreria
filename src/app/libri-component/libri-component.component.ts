75766104383abc40a456a19df3f1cee7b1d4f9ae
Pasquale <pasquale_florio@live.it>
2020-06-20 15:39:37 +0200

    init dettagli libro

--- src/app/libri-component/libri-component.component.ts
+++ src/app/libri-component/libri-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LibriService} from '../services/libri.service';
 
 @Component({
@@ -8,16 +8,27 @@ import {LibriService} from '../services/libri.service';
 })
 export class LibriComponentComponent implements OnInit {
 
-  constructor(private libriService: LibriService) { }
+  constructor(private libriService: LibriService) {
+  }
 
   listaLibri = 'Elenco totale libri';
   // tslint:disable-next-line:ban-types
   libri;
   libroSelected = null;
+
   ngOnInit() {
     this.libriService.getLibri().subscribe(
       response => {
         this.libri = response;
+      }
+    );
+  }
+
+  dettagliLibro(libro) {
+    const index = this.libri.indexOf(libro);
+    this.libriService.getLibro(index).subscribe(
+      response => {
+        this.libroSelected = response;
       }
     );
   }
