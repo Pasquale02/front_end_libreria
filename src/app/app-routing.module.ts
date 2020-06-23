bcc1eb4cb4da7f4b4c7cfa8374acc355be15bc30
Pasquale <pasquale_florio@live.it>
2020-06-23 14:07:39 +0200

    update url path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,9 @@ import { ArticlesComponent } from './articles/articles/articles.component'
 import { ArticleDetailsComponent } from './articles/article-details/article-details.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'autori', component: HomeComponent },
+  { path: 'libri', component: AboutComponent },
+  { path: '', redirectTo: '/autori', pathMatch: 'full' },
   { path: 'articles', component: ArticlesComponent },
   { path: 'article/:id', component: ArticleDetailsComponent },
   { path: '**', component: PageNotFoundComponent }
